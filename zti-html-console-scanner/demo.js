9bd78ada606eb84998f11945273452198a10201f
Cvazer <zhirnitoni@gmail.com>
2018-01-31 12:27:49 +0300

    zhcsv1.1 update

--- zti-html-console-scanner/demo.js
+++ zti-html-console-scanner/demo.js
@@ -1,12 +1,15 @@
 ;(function () {
     let scanner = window.scanner;
     scanner.set({printCommands: false});
-    scanner.addCommand("test", function (params) {
-        if (params[scanner.defaultKey] === undefined) {return}
-        let s = "";
-        params[scanner.defaultKey].forEach(value => {
-            s += value + " ";
-        });
-        scanner.print(s.trim());
+    scanner.addCommand({
+        name: "test",
+        consume: function (params) {
+            if (params[scanner.defaultKey] === undefined) {return}
+            let s = "";
+            params[scanner.defaultKey].forEach(value => {
+                s += value + " ";
+            });
+            scanner.print(s.trim());
+        }
     });
 })();
